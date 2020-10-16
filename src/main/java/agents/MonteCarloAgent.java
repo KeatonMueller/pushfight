58cfa1822b7beb139fa3afc3e6c2cd198a50183c
Keaton Mueller <keaton.mueller@yale.edu>
2020-10-16 14:10:19 -0400

    add basic MCTS implementation

---
+++ src/main/java/agents/MonteCarloAgent.java
@@ -0,0 +1,320 @@
+package main.java.agents;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+
+import main.java.board.Bitboard;
+import main.java.board.BitboardUtils;
+
+/**
+ * Agent to find next move using Monte-Carlo Tree Search.
+ * 
+ * Adapted from my own homework from CPSC 474.
+ */
+public class MonteCarloAgent extends Agent {
+    /**
+     * Data structure to maintain the game tree that the agent explores
+     */
+    private class Tree {
+        Node root; // root of tree
+        Map<Bitboard, Node> map; // map from board positions to corresponding Nodes
+
+        /**
+         * Initialize game tree with given board position at the root
+         * 
+         * @param rootPos Root board position
+         * @param turn    Turn indicator
+         */
+        public Tree(Bitboard rootPos, int turn) {
+            root = new Node(rootPos, turn);
+            map = new HashMap<>();
+            map.put(rootPos, root);
+        }
+
+        /**
+         * Get the Node corresponding to the given board state, or create a new one if none.
+         * 
+         * @param board The board to get the node for
+         * @param turn  Turn indicator
+         * @return The corresponding Node
+         */
+        public Node getNode(Bitboard board, int turn) {
+            if (map.containsKey(board)) {
+                return map.get(board);
+            }
+            Node node = new Node(board, turn);
+            map.put(board, node);
+            return node;
+        }
+    }
+
+    /**
+     * Data structure for individual nodes in the game tree
+     */
+    private class Node {
+        private List<Node> children; // (visited) children of current node
+        private List<Node> parents; // list of parents (there can be more than one)
+        private Node chosenParent; // specific parent chosen during a playout
+
+        private Bitboard board; // board state associated with this node
+        private boolean isTerminal; // whether or not node is terminal
+        private boolean isFullyExpanded; // whether or not node is fully expanded
+        private List<Bitboard> unexplored; // list of unexplored children
+        private int T; // number of times current node has been visited
+        private Map<Node, Stats> childToStats; // map from child node to stats
+
+        /**
+         * Initialize new Node for given board state
+         * 
+         * @param board Board state
+         * @param turn  Turn indicator
+         */
+        public Node(Bitboard board, int turn) {
+            this.board = board;
+            this.isTerminal = BitboardUtils.checkWinner(board) != -1;
+            this.unexplored = new ArrayList<>(BitboardUtils.getNextStates(board, turn));
+            this.isFullyExpanded = this.unexplored.size() == 0;
+            this.children = new ArrayList<>();
+            this.parents = new ArrayList<>();
+            this.T = 0;
+            this.childToStats = new HashMap<>();
+        }
+
+        @Override
+        public int hashCode() {
+            return board.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+
+            Node other = (Node) obj;
+            return this.board.equals(other.board);
+        }
+    }
+
+    /**
+     * Stats object to track statistics for EDGES in the game tree
+     */
+    private class Stats {
+        private int n; // number of time edge has been taken
+        private int r; // aggregate reward seen along edge
+
+        /**
+         * Initialize Stats object with zero values
+         */
+        public Stats() {
+            this.n = 0;
+            this.r = 0;
+        }
+    }
+
+    private long timeLimit; // time allowed to explore game tree
+    private Random rand; // Random object used for random playouts
+    private int turn; // turn indicator
+
+    /**
+     * Initialize Monte-Carlo Tree Search agent with given time limit
+     * 
+     * @param timeLimit Max time (in seconds) allowed per move
+     */
+    public MonteCarloAgent(long timeLimit) {
+        this.timeLimit = timeLimit * 1000; // convert seconds to milliseconds
+        this.rand = new Random();
+    }
+
+    public Bitboard getNextState(Bitboard board, int turn) {
+        this.turn = turn;
+        Tree tree = new Tree(board, this.turn);
+        Node leaf;
+        int result;
+        long startTime = System.currentTimeMillis();
+        while (System.currentTimeMillis() - startTime < timeLimit) {
+            leaf = traverse(tree);
+            result = playout(leaf);
+            updateStats(leaf, result);
+        }
+        return getBestState(tree.root);
+    }
+
+    /**
+     * Traverse the game tree by following UCT algorithm
+     * 
+     * @param tree Tree to traverse
+     * @return Leaf Node at the bottom of the traversal
+     */
+    private Node traverse(Tree tree) {
+        Node node = tree.root;
+        Node nextNode;
+
+        // follow UCT until you find a non-fully-expanded node
+        while (node.isFullyExpanded && !node.isTerminal) {
+            nextNode = bestUCT(node);
+            nextNode.chosenParent = node;
+            node = nextNode;
+            turn = 1 - turn;
+        }
+        if (node.isTerminal) {
+            return node;
+        }
+
+        // randomly choose an unexplored child
+        Bitboard nextBoard = node.unexplored.remove(rand.nextInt(node.unexplored.size()));
+        nextNode = tree.getNode(nextBoard, turn);
+        node.isFullyExpanded = node.unexplored.size() == 0;
+
+        // add child to node's children and initialize new edge Stats
+        node.children.add(nextNode);
+        node.childToStats.put(nextNode, new Stats());
+
+        // add node to child's parents and set the chosen parent
+        nextNode.parents.add(node);
+        nextNode.chosenParent = node;
+
+        return nextNode;
+    }
+
+    /**
+     * Randomly playout from the given Node to a terminal state
+     * 
+     * @param node Node to playout from
+     * @return Result of playout (1 if p1 win, -1 if p2 win)
+     */
+    private int playout(Node node) {
+        Bitboard board = new Bitboard(node.board);
+        int winner;
+        while (true) {
+            winner = BitboardUtils.checkWinner(board);
+            if (winner != -1) {
+                if (winner == 0)
+                    return 1;
+                return -1;
+            }
+
+            RandomAgent.randomMove(board, turn, rand);
+            turn = 1 - turn;
+        }
+    }
+
+    /**
+     * Traverse up the game tree updating node and edge statistics
+     * 
+     * @param node   Node to traverse from
+     * @param result Game result to propagate
+     */
+    private void updateStats(Node node, int result) {
+        // increment number of visits
+        node.T += 1;
+
+        // stop if reached root
+        if (node.parents.size() == 0)
+            return;
+
+        Node parent = node.chosenParent;
+        node.chosenParent = null;
+
+        // update edge statistics
+        Stats stats = parent.childToStats.get(node);
+        stats.n += 1;
+        stats.r += result;
+
+        // recurse
+        updateStats(parent, result);
+    }
+
+    /**
+     * Get the next child from the given node based on UCT
+     * 
+     * @param node Node to perform UCT on
+     * @return Next node to explore
+     */
+    private Node bestUCT(Node node) {
+        Node bestNode = null;
+        double bestUCB, ucb;
+        int numerator, denominator;
+        Stats stats;
+        if (turn == 0) {
+            bestUCB = -Double.MAX_VALUE;
+            for (Node child : node.children) {
+                numerator = 0;
+                denominator = 0;
+                for (Node parent : child.parents) {
+                    stats = parent.childToStats.get(child);
+                    numerator += stats.r;
+                    denominator += stats.n;
+                }
+                stats = node.childToStats.get(child);
+                ucb = ((double) numerator / denominator)
+                        + (2 * Math.log(node.T) / Math.pow(stats.n, 0.5));
+
+                if (ucb > bestUCB) {
+                    bestUCB = ucb;
+                    bestNode = child;
+                }
+            }
+        } else {
+            bestUCB = Double.MAX_VALUE;
+            for (Node child : node.children) {
+                numerator = 0;
+                denominator = 0;
+                for (Node parent : child.parents) {
+                    stats = parent.childToStats.get(child);
+                    numerator += stats.r;
+                    denominator += stats.n;
+                }
+                stats = node.childToStats.get(child);
+                ucb = ((double) numerator / denominator)
+                        - (2 * Math.log(node.T) / Math.pow(stats.n, 0.5));
+
+                if (ucb < bestUCB) {
+                    bestUCB = ucb;
+                    bestNode = child;
+                }
+            }
+        }
+        return bestNode;
+    }
+
+    /**
+     * Get the best next state from the given node based on observed reward
+     * 
+     * @param node Node to get best move from
+     * @return Bitboard of next state with the highest observed reward
+     */
+    private Bitboard getBestState(Node node) {
+        Node bestNode = null;
+        double bestR, r;
+        Stats stats;
+        if (turn == 0) {
+            bestR = -Double.MAX_VALUE;
+            for (Node child : node.childToStats.keySet()) {
+                stats = node.childToStats.get(child);
+                r = (double) stats.r / stats.n;
+                if (r > bestR) {
+                    bestR = r;
+                    bestNode = child;
+                }
+            }
+        } else {
+            bestR = Double.MAX_VALUE;
+            for (Node child : node.childToStats.keySet()) {
+                stats = node.childToStats.get(child);
+                r = (double) stats.r / stats.n;
+                if (r < bestR) {
+                    bestR = r;
+                    bestNode = child;
+                }
+            }
+        }
+        return bestNode.board;
+    }
+}
--- src/main/java/agents/RandomAgent.java
+++ src/main/java/agents/RandomAgent.java
@@ -1,10 +1,12 @@
 package main.java.agents;
 
+import java.util.List;
 import java.util.Set;
 import java.util.Random;
 
 import main.java.board.Bitboard;
 import main.java.board.BitboardUtils;
+import main.java.game.GameUtils;
 
 public class RandomAgent extends Agent {
     Random rand;
@@ -23,5 +25,63 @@ public class RandomAgent extends Agent {
             idx++;
         }
         return null;
+    }
+
+    /**
+     * Perform a random move on the given board without instantiating a RandomAgent instance. Also
+     * eliminates the need to generate every possible move, since it returns as soon as it generates
+     * a single valid move.
+     * 
+     * @param board Board to perform random move on
+     * @param turn  Turn indicator
+     * @param rand  Instance of the Random class
+     */
+    public static void randomMove(Bitboard board, int turn, Random rand) {
+        Bitboard initState = board.getState();
+        int i, push;
+        while (true) {
+            for (i = 0; i < GameUtils.NUM_SLIDES; i++) {
+                BitboardUtils.decodeSlide(board, randomSlide(board, turn, rand), turn);
+            }
+
+            push = randomPush(board, turn, rand);
+            if (push == -1) {
+                board.restoreState(initState);
+                continue;
+            }
+            BitboardUtils.decodePush(board, push, turn);
+            return;
+        }
+    }
+
+    /**
+     * Perform a random slide on the given board
+     * 
+     * @param board Board to perform random slide on
+     * @param turn  Turn indicator
+     * @param rand  Instance of the Random class
+     * @return A random bit mask corresponding to a valid sliding action
+     */
+    public static int randomSlide(Bitboard board, int turn, Random rand) {
+        List<Integer> actions = BitboardUtils.getSlideActions(board, turn);
+        return actions.get(rand.nextInt(actions.size()));
+    }
+
+    /**
+     * Perform a random push on the given board
+     * 
+     * @param board Board to perform random push on
+     * @param turn  Turn indicator
+     * @param rand  Instanc eof the Random class
+     * @return A random bit mask corresponding to a valid push action, or -1 if no pushes possible
+     *         from the given position
+     */
+    public static int randomPush(Bitboard board, int turn, Random rand) {
+        List<Integer> actions = BitboardUtils.getPushActions(board, turn);
+
+        if (actions.size() == 0)
+            return -1;
+
+        return actions.get(rand.nextInt(actions.size()));
     }
 }
